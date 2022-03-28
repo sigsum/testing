8be06133b33637ae060529123ca4b598f0baa7d1
Rasmus Dahlberg <rasmus@mullvad.net>
2022-03-28 14:57:04 +0200

    bring in sigsum-lib-go as dep

--- hola/go.mod
+++ hola/go.mod
@@ -1,3 +1,5 @@
 module git.sigsum.org/testing/hola
 
 go 1.17
+
+require git.sigsum.org/sigsum-lib-go v0.0.2 // indirect
--- hola/main.go
+++ hola/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+
+	"git.sigsum.org/sigsum-lib-go/pkg/hex"
 )
 
 func main() {
-	log.Printf("hola")
+	log.Printf("hola: %s", hex.Serialize([]byte{0,1,2,3}))
 }
